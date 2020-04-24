f2015f1315d11a6c9a96a682c51fd3918cf8ab9f
LydiaWozniak <lydia.wozniak@bbc.co.uk>
2020-04-24 17:11:26 +0100

    Using class constructors

--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
 
 public class Main {
-   public static void main(String[] args) {
-      // Primitive datatypes
-      int myFavouriteNumber = 4;
-      double someDecimal = 3.4;
-      boolean amIHungry = true;
-      String aLotOfCharacters = "Hello World!";
-      char oneCharacter = 'A';
 
-      // Prints "Hello, World" in the terminal window.
-      System.out.println(aLotOfCharacters);
+   int myFavouriteNumber;
+   double someDecimal;
+   boolean amIHungry;
+   String aLotOfCharacters;
+   char aSingleCharacter;
+
+   public Main(int number, double decimal, boolean hungry, String word, char letter) {
+      myFavouriteNumber = number;
+      someDecimal = decimal;
+      amIHungry = hungry;
+      aLotOfCharacters = word;
+      aSingleCharacter = letter;
+
+   }
+
+   public static void main(String[] args) {
+
+      Main myself = new Main(4, 3.4, true, "Hello World!", 'A');
+
+      System.out.println("Am I hungry " + myself.amIHungry);
 
    }
 }
