b63bbe51c40da7bfe135b021f432dc6ed8d3a712
LydiaWozniak <lydia.wozniak@bbc.co.uk>
2020-04-24 14:55:37 +0100

    Add variable as examples of datatypes

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
 
 public class Main {
    public static void main(String[] args) {
+      // Primitive datatypes
+      int myFavouriteNumber = 4;
+      double someDecimal = 3.4;
+      boolean amIHungry = true;
+      String aLotOfCharacters = "Hello World!";
+      char oneCharacter = 'A';
+
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
+      System.out.println(aLotOfCharacters);
+
    }
 }
